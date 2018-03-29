2d8d8d36dd14139c9afdfeb7b8b3f15bae7c97fe
Xiaoyi <33134727+singingsea@users.noreply.github.com>
2018-03-29 11:00:51 -0400

    tune the filters
    we have three filters can be used
    1. CI
    2. sky-imager
    3. OMI

--- check_ZS_DS_skyimager_OMI.m
+++ check_ZS_DS_skyimager_OMI.m
@@ -3,17 +3,45 @@ load('C:\Projects\Zenith_NO2\matlab2.mat');
 
 DU = 2.6870e+16;
 
+data = data_unfiltered;
+use_CI_filter = true;
+use_sky_imager_filter = true;
+use_OMI_filter = true;
 
-% general filters
+% CI filters
+if use_CI_filter
+    TF_CI = data.CI < (mean(data.CI)-std(data.CI));
+    data(TF_CI,:) = [];
+end
+
+% sky-imager filters
+if use_sky_imager_filter
+    TF = isnan(data.iSunStrength);
+    data(TF,:) = [];
+    TF_iSunStrength = data.iSunStrength < (mean(data.iSunStrength) - std(data.iSunStrength));
+    data(TF_iSunStrength,:) = [];
+
+    TF_iPctOpq  = data.iPctOpq  > (mean(data.iPctOpq ) + std(data.iPctOpq ));
+    data(TF_iPctOpq ,:) = [];
+
+    TF_iPctThin  = data.iPctThin  > (mean(data.iPctThin ) + std(data.iPctThin ));
+    data(TF_iPctThin ,:) = [];
+end
+
+
+% OMI data general filters
 TF_OMI = isnan(data.ColumnAmountNO2);
 data(TF_OMI,:) =[];
 TF_NO2_amount = data.ColumnAmountNO2./DU > -1e10;
 data = data(TF_NO2_amount,:);
-TF_cloud = data.CloudFraction < 0.3;
-data = data(TF_cloud,:);
-TF_distance = data.distance < 50000;
-data = data(TF_distance,:);
-
+TF_VcdQualityFlags = data.VcdQualityFlags ~= 0; % see OMI NO2 L2 document
+data(TF_VcdQualityFlags,:) =[];
+if use_OMI_filter
+    TF_cloud = data.CloudFraction < 0.3;
+    data = data(TF_cloud,:);
+    TF_distance = data.distance < 50000;
+    data = data(TF_distance,:);
+end
 
 TF1 = strcmp(data.mean_type,"closest");
 TF2 = strcmp(data.mean_type,"dis_f1");
@@ -34,11 +62,18 @@ plot(data.DateDDMMYYYY_Timehhmmss(TF3,:),data.ColumnAmountNO2Strat(TF3,:)./DU,'d
 ylabel('OMI NO_2 [DU]');
 legend('DS','ZS','NO2 closest','NO2 20km','NO2 50km','NO2_S_t_r_a_t closest','NO2_S_t_r_a_t 20km','NO2_S_t_r_a_t 50km');
 R1 = corr(data.VCD(TF1,:),data.ColumnAmountNO2(TF1,:)./DU);
-disp(['R1 = ' num2str(R1)]);
+disp(['R1 (DS vs. OMI closest) = ' num2str(R1)]);
 R2 = corr(data.VCD(TF2,:),data.ColumnAmountNO2(TF2,:)./DU);
-disp(['R2 = ' num2str(R2)]);
+disp(['R2 (DS vs. OMI dis f1) = ' num2str(R2)]);
 R3 = corr(data.VCD(TF3,:),data.ColumnAmountNO2(TF3,:)./DU);
-disp(['R3 = ' num2str(R3)]);
+disp(['R3 (DS vs. OMI dis f2) = ' num2str(R3)]);
+
+R1 = corr(data.ndacc_vcd(TF1,:),data.ColumnAmountNO2(TF1,:)./DU);
+disp(['R1 (ZS vs. OMI closest) = ' num2str(R1)]);
+R2 = corr(data.ndacc_vcd(TF2,:),data.ColumnAmountNO2(TF2,:)./DU);
+disp(['R2 (ZS vs. OMI dis f1) = ' num2str(R2)]);
+R3 = corr(data.ndacc_vcd(TF3,:),data.ColumnAmountNO2(TF3,:)./DU);
+disp(['R3 (ZS vs. OMI dis f2) = ' num2str(R3)]);
 
 %% OMI vs DS (closest)
 TF_fig = TF1;
@@ -132,7 +167,7 @@ xlabel('ZS VCD [DU]');
 legend('data density','1-on-1','y = a*x+b', 'y = a*x');
 title('dis f1');
 R = corr(x,y);
-text(0,2,['R = ' num2str(R)]);
+textbp(['R = ' num2str(R)]);
 grid on;
 
 %% OMI vs ZS (dis f2)
@@ -151,7 +186,7 @@ xlabel('ZS VCD [DU]');
 legend('data density','1-on-1','y = a*x+b', 'y = a*x');
 title('dis f2');
 R = corr(x,y);
-text(0,2,['R = ' num2str(R)]);
+textbp(['R = ' num2str(R)]);
 grid on;
 
 %% 
@@ -162,7 +197,10 @@ mdl = fitnlm(x,y,modelfun,beta0);
 
 new_x = [-10;10];
 new_y = predict(mdl,new_x);
-plot(new_x,new_y);
+plot(new_x,new_y,'r');
+b1 = mdl.Coefficients.Estimate;
+textbp(['y = ' num2str(b1) '*x'],'color','r' );
+
 
 
 %% 
@@ -172,4 +210,6 @@ intercept = mdl.Coefficients.Estimate(1);
 slop = mdl.Coefficients.Estimate(2);
 new_x = [-10;10];
 new_y = predict(mdl,new_x);
-plot(new_x,new_y);
+plot(new_x,new_y,'b');
+textbp(['y = ' num2str(slop) '*x + ' num2str(intercept)],'color','b'  );
+textbp(['N = ' num2str(numel(x))])
